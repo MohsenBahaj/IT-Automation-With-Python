d9ca032b511ad90412c401a32f171243857ad547
unknown <hgvrl121@gmail.com>
2024-01-04 22:58:37 +0530

    i have modified python file

--- emp.py
+++ emp.py
@@ -3,8 +3,8 @@ import csv
 
 
 def read_employees(csv_file_location):
-    csv.register_dialect('empDialect')#, skipinitialspace=True, strict=True
-    employee_file = csv.DictReader(open(csv_file_location))#, dialect='empDialect'
+    csv.register_dialect('empDialect', skipinitialspace=True, strict=True)
+    employee_file = csv.DictReader(open(csv_file_location), dialect='empDialect')
     employee_list = []
     for data in employee_file:
         employee_list.append(data)
